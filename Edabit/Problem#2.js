daf6eb1d47ac18c372e318917875b1ea9f5438ba
Eli_ <63687784+Daegatoya@users.noreply.github.com>
2021-10-07 17:58:59 -0400

    Create Problem#2.js

---
+++ Edabit/Problem#2.js
@@ -0,0 +1,16 @@
+//Check if the number is higher than the initial number when the 2 digits are switched.
+function NumFalseTrue(input){
+    var x = input.toString();
+    var y = x.split("")
+    var arr = [y[1], y[0]];
+    var NewArr = arr.join("");
+    
+    if(NewArr > input){
+        return console.log(false)
+    }
+    else{
+        return console.log(true)
+    }
+}
+
+NumFalseTrue(41)
