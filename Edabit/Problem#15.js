a2110e255c567452e5d7309e74f2071e8e6ac4ce
Eli_ <63687784+Daegatoya@users.noreply.github.com>
2021-10-07 18:12:45 -0400

    Create Problem#15.js

---
+++ Edabit/Problem#15.js
@@ -0,0 +1,11 @@
+//Simply sort an array.
+
+function Sorting(input){
+input.toString();
+var y = input.sort(function (a, b){
+    return a - b;
+})
+console.log(y)
+}
+
+Sorting([1, 8, [4], [5], 2])
